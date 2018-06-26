e9d8902dc73050c367653ffef71048e94df0ff76
M00NBOY <zeroualmahel@gmail.com>
2018-06-26 15:06:31 +0200

    add Symfony structure

--- xenosAPI/config/bundles.php
+++ xenosAPI/config/bundles.php
@@ -14,4 +14,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];

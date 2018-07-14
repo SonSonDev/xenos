25bac3e0eb2fe41294309ee3db45838bc8297796
Theodore <theodoreyip19@gmail.com>
2018-07-14 16:45:49 +0200

    added data to explore

--- xenosVUE/src/views/exploreSteps/step1.vue
+++ xenosVUE/src/views/exploreSteps/step1.vue
@@ -30,20 +30,20 @@ export default {
   components: { E1main, E1slider },
   data: function() {
     return {
-      planets: [],
+      planets: [{"@id":"/api/planets/1","@type":"Planet","id":1,"name":"Mars","description":"Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury.","img":null,"colorStart":"#47332C","colorEnd":"#FF956E","video":"https://www.youtube.com/embed/NXbCNAIIAxw","zones":["/api/zones/1","/api/zones/2","/api/zones/3"],"temperature":-60,"gravity":3.711,"distance":"225 million","radius":"3 390"},{"@id":"/api/planets/2","@type":"Planet","id":2,"name":"Venus","description":"Venus is the second planet from the Sun. It  rotates in the opposite direction to most other planets.","img":null,"colorStart":"#A47B4D","colorEnd":"#D5C2A1","video":"https://www.youtube.com/embed/yzqbN6z8ncc","zones":["/api/zones/4","/api/zones/5","/api/zones/6"],"temperature":465,"gravity":8.87,"distance":"40 million","radius":"6 052"},{"@id":"/api/planets/3","@type":"Planet","id":3,"name":"Mercury","description":"Mercury is the smallest planet in the Solar System. Its orbital period is also the shortest.","img":null,"colorStart":"#3F3F3F","colorEnd":"#CDCDCD","video":"https://www.youtube.com/embed/m3ZUhpisWeQ","zones":["/api/zones/7","/api/zones/8","/api/zones/9"],"temperature":166.85,"gravity":3.7,"distance":"90 million","radius":"2 440"},{"@id":"/api/planets/4","@type":"Planet","id":4,"name":"Europa","description":"Europa is the smallest of the four Galilean moons orbiting Jupiter, and the sixth-closest to the planet.","img":null,"colorStart":"#937C53","colorEnd":"#959C94","video":"https://www.youtube.com/embed/kz9VhCQbPAk","zones":["/api/zones/10","/api/zones/11","/api/zones/12"],"temperature":-190,"gravity":1.314,"distance":"628.3 millions","radius":"1561"}],
       current: 0,
       turn: 0,
     }
   },
   name: 'exploreMain',
   created: function () {
-    fetch(this.$baseUrl + '/api/planets', {
-      method: 'GET'
-    })
-    .then(response => response.json())
-    .then(data => {
-      this.planets = data['hydra:member']
-    })
+    // fetch(this.$baseUrl + '/api/planets', {
+    //   method: 'GET'
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //   this.planets = data['hydra:member']
+    // })
   },
   mounted: function () {
   },
--- xenosVUE/src/views/exploreSteps/step2.vue
+++ xenosVUE/src/views/exploreSteps/step2.vue
@@ -31,25 +31,30 @@ export default {
   props: ['planet'],
   data: function() {
     return {
-      zones: [],
+      zones: {
+        Mars: [{"@id":"/api/zones/1","@type":"Zone","id":1,"planet":"/api/planets/1","name":"Vastitas Borealis","description":"Vastitas Borealis is the largest lowland region of Mars. It is in the northerly latitudes of the planet.","price":200,"bookings":["/api/bookings/14"]},{"@id":"/api/zones/2","@type":"Zone","id":2,"planet":"/api/planets/1","name":"Tharsis","description":"Tharsis is a vast volcanic plateau centered near the equator in the western hemisphere of Mars.","price":300,"bookings":["/api/bookings/6","/api/bookings/8"]},{"@id":"/api/zones/3","@type":"Zone","id":3,"planet":"/api/planets/1","name":"Hellas Planitia","description":"Hellas Planitia is a plain located  in the southern hemisphere of the planet Mars.","price":250,"bookings":["/api/bookings/10","/api/bookings/13"]}],
+        Europa: [{"@id":"/api/zones/10","@type":"Zone","id":10,"planet":"/api/planets/4","name":"Ridged plains","description":"The ridged plains are vast icy plains.","price":3900,"bookings":["/api/bookings/7"]},{"@id":"/api/zones/11","@type":"Zone","id":11,"planet":"/api/planets/4","name":"Knobby Chaos","description":"The Knobby Chaos region has a steep and chaotic terrain.","price":4600,"bookings":["/api/bookings/5"]},{"@id":"/api/zones/12","@type":"Zone","id":12,"planet":"/api/planets/4","name":"High Albedo Bands","description":"High Albedo Bands is the name of a relief line on the South of Europa.","price":4000,"bookings":["/api/bookings/12"]}],
+        Mercury: [{"@id":"/api/zones/7","@type":"Zone","id":7,"planet":"/api/planets/3","name":"Borealis","description":"Borealis is a quadrangle on Mercury surrounding the north pole down to 65° latitude.","price":750,"bookings":[]},{"@id":"/api/zones/8","@type":"Zone","id":8,"planet":"/api/planets/3","name":"Victoria","description":"The Victoria quadrangle is a region on Mercury from 0 to 90° longitude and 20 to 70 ° latitude.","price":700,"bookings":["/api/bookings/9"]},{"@id":"/api/zones/9","@type":"Zone","id":9,"planet":"/api/planets/3","name":"Shakespeare","description":"The Shakespeare quadrangle is a region of Mercury running from 90 to 180° longitude and 20 to 70° latitude.","price":700,"bookings":[]}],
+        Venus: [{"@id":"/api/zones/4","@type":"Zone","id":4,"planet":"/api/planets/2","name":"Ishtar Terra","description":"Ishtar Terra is one of two main highland regions on the planet Venus.","price":800,"bookings":[]},{"@id":"/api/zones/5","@type":"Zone","id":5,"planet":"/api/planets/2","name":"Aphrodite Terra","description":"Aphrodite Terra is a highland region on Venus, near the equator.","price":850,"bookings":[]},{"@id":"/api/zones/6","@type":"Zone","id":6,"planet":"/api/planets/2","name":"Alpha Regio","description":"Alpha Regio is a region of the planet Venus extending for about 1500 km centered at 22°S, 5°E.","price":800,"bookings":["/api/bookings/11"]}]
+      },
       current: null,
       choice: null
     }
   },
   created: function () {
     this.choice = this.planet
-    if (!this.choice) {
-      fetch(this.$baseUrl + '/api/planets', {method: 'GET'})
-      .then(response => response.json())
-      .then(data => {
-        this.choice = data['hydra:member'].filter((item) => {
-          return (item.name === this.$route.params.planet)
-        })[0]
-      })
-      .then(() => {
-        this.fetchZones()
-      })
-    }
+    // if (!this.choice) {
+      // fetch(this.$baseUrl + '/api/planets', {method: 'GET'})
+      // .then(response => response.json())
+      // .then(data => {
+      //   this.choice = data['hydra:member'].filter((item) => {
+      //     return (item.name === this.$route.params.planet)
+      //   })[0]
+      // })
+      // .then(() => {
+      //   this.fetchZones()
+      // })
+    // }
     this.fetchZones()
   },
   methods: {
@@ -57,15 +62,16 @@ export default {
       this.current = id
     },
     fetchZones: function () {
-      fetch(this.$baseUrl + '/api/zones', {
-        method: 'GET'
-      })
-      .then(response => response.json())
-      .then(data => {
-        this.zones = data['hydra:member'].filter((item) => {
-          return (item.planet === this.choice['@id'])
-        })
-      })
+      // fetch(this.$baseUrl + '/api/zones', {
+      //   method: 'GET'
+      // })
+      // .then(response => response.json())
+      // .then(data => {
+      //   this.zones = data['hydra:member'].filter((item) => {
+      //     return (item.planet === this.choice['@id'])
+      //   })
+      // })
+      this.zones = this.zones[this.choice.name]
     }
   }
 }
